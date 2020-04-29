3d7b1614a8a26f284a19ea41675fe00df7b5582b
Qaisarm <qmphy99@gmail.com>
2020-04-29 14:12:44 +0200

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [StringLength(30, ErrorMessage = "Maximum 30 charcters")]
+        public string Name { get; set; }
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Order { get; set; }
+
+        [Required]
+        public string Categor { get; set; }
+        
+        public string Shelf { get; set; }
+        [Range(0,10000)]
+        public int Count { get; set; }
+        public string Descrip { get; set; }
+    }                        
+}
